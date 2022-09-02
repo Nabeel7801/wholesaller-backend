6351e45e3f079109949aac3c6995efb44b72ff5c
Nabeel Ahmed <nabeel.ahmed0026@gmail.com>
2022-09-02 22:18:44 +0500

    Bugs Fixed

---
+++ build/static/js/684.de71d886.chunk.js
@@ -0,0 +1,18 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[684],{12684:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});t(72791);var r=t(80184);function u(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=684.de71d886.chunk.js.map
---
+++ build/static/js/881.e7f1c554.chunk.js
@@ -0,0 +1,18 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[881],{41881:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});t(72791);var r=t(80184);function u(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=881.e7f1c554.chunk.js.map
