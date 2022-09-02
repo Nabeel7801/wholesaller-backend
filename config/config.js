22e5ec84e2c0ea857c34f0ad120d55620b799a72
Nabeel Ahmed <nabeel.ahmed0026@gmail.com>
2022-09-02 19:31:56 +0500

    Production Website

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ////mongoURI: "mongodb+srv://saadi:saadi@cluster0-znryv.mongodb.net/audiomac",
-  mongoURI: "mongodb://encodersoft1:Arbisoftisbest1@194.233.77.156:27017/wholesaller?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-
-
-
- 
-};
--- models - old/email/emailVerify.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const emailVerifySchema = new mongoose.Schema({
-
-    email: { type: String, },
-
-});
-
-module.exports = mongoose.model("emailVerify", emailVerifySchema);
